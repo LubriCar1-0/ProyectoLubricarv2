fa22200d7df806e51fe7cada04ff092f90325b10
EstebanDesimone <estebandesimone2000@gmail.com>
2024-09-24 17:22:33 -0300

    Cadena de conexion

---
+++ Datos/Conectar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Conectar
+    {
+        DataTable dt = new DataTable();
+        SqlDataReader leer;
+        public static SqlCommand comando = new SqlCommand();
+        public static SqlConnection conexion = new SqlConnection("server= localhost\\SQLEXPRESS; database= Lubri-Car; integrated security = true");
+        private static void conectar()
+        {
+            conexion.Open();
+        }
+
+        private static void desconectar()
+        {
+            conexion.Close();
+        }
+    }
+}
